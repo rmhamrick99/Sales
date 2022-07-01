3a2debf8acf2bfb190c2564c505ad286bf48f5a6
NadaRenate <72452095+rmhamrick99@users.noreply.github.com>
2022-07-01 17:51:56 -0400

    initial commit

---
+++ Salescpp.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	double *sales = nullptr,
+		total = 0.0,
+		average;
+
+	int numDays,
+		count;
+	
+	cout << "How many days of sales amounts do you wish to process? ";
+	cin >> numDays;
+	
+	sales = new double[numDays];
+	
+	cout << "Enter the sales amounts below.\n";
+	for (count = 0;count < numDays; count++)
+	{
+		cout << "Day " << (count + 1) << ": ";
+		cin >> sales[count];
+
+	}
+	for (count = 0;count < numDays; count++)
+	{
+		total += sales[count];
+	}
+	average = total / numDays;
+
+	cout << fixed << showpoint << setprecision(2);
+	cout << "\n\nTotal Sales: $" << total << endl;
+	cout << "Average Sales: $" << average <<endl;
+
+	delete[] sales;
+	sales = nullptr;
+
+	system("pause");
+
+	return 0;
+}
